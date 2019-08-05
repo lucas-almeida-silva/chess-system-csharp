436f1a2b9605b6402a67b95ea4c5ad11083b2f84
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2019-08-05 16:10:21 -0300

    Jogada especial promocao

--- xadrez_console/xadrez/PartidaXadrez.cs
+++ xadrez_console/xadrez/PartidaXadrez.cs
@@ -141,6 +141,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            //#jogada especial - promocao
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -159,8 +174,6 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             //#jogada especial - en passant
             if(p is Peao && destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2)
